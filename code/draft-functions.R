2eeff2abedd0042dbbcfb247f88024c1c6c3f007
Jesse Lecy <jdlecy@gmail.com>
2020-06-18 18:39:21 -0700

    updated draft function and maps

--- code/draft-functions.R
+++ code/draft-functions.R
@@ -1,5 +1,14 @@
 ###############
 
+# install necessary packages
+
+required.packages <-
+c( "geojsonio", "sp", "sf", "mclust", 
+   "tmap", "ggplot2", "ggthemes", 
+   "dplyr", "pander", "tidycensus", 
+   "cartogram", "maptools" ) 
+install.packages( required.packages )
+
 library( geojsonio )   # read shapefiles
 library( sp )          # work with shapefiles
 library( sf )          # work with shapefiles - simple features format
@@ -19,6 +28,7 @@ library(here)
 
 
 # folder should be where RMD file is
+# "C:/Users/jdlecy/Dropbox/00 - PEDA/00 - GITHUB/usa-dorling-shapefiles"
 setwd( "usa-dorling-shapefiles/code" )
 
 # load data
@@ -40,7 +50,7 @@ census.dat$tractid <- as.numeric( as.character( census.dat$tractid ) )
 
 options( tigris_use_cache = TRUE )
 
-build_dorling_metro <- function( cbsa.name )
+build_dorling_metro <- function( cbsa.name, include.plot=TRUE )
 {
 
 	these.counties  <- crosswalk$cbsaname == cbsa.name
@@ -54,65 +64,77 @@ build_dorling_metro <- function( cbsa.name )
 
 	for( i in 1:length(these.fips) )
 	{
-	  d.temp <- 
-	  get_acs( geography = "tract", 
-		   variables = "B01003_001",  # population
-		   state = state.fips[i], 
-		   county = county.fips[i], 
-		   geometry = TRUE ) %>%
-	  select( GEOID, estimate ) %>%
-	  rename( POP = estimate )
+	d.temp <- 
+	get_acs( geography = "tract", 
+	   variables = "B01003_001",  # population
+	   state = state.fips[i], 
+	   county = county.fips[i], 
+	   geometry = TRUE ) %>%
+	select( GEOID, estimate ) %>%
+	rename( POP = estimate )
 
-	  d.sf <- rbind( d.sf, d.temp )
+	d.sf <- rbind( d.sf, d.temp )
 	}
 
 
-    # merge shapefile data with census data in new dataframe:
-    # fix leading zeros problem
-    # census.dat$tractid <- as.numeric( as.character( census.dat$tractid ) )
-    d.sf$GEOID <- as.numeric( as.character( d.sf$GEOID ) )
-
-
+	# merge shapefile data with census data in new dataframe:
+	# fix leading zeros problem
+	# census.dat$tractid <- as.numeric( as.character( census.dat$tractid ) )
+	d.sf$GEOID <- as.numeric( as.character( d.sf$GEOID ) )
 	d.sf <- merge( d.sf, census.dat, by.x="GEOID", by.y="tractid", all.x=T )
-    
-	# save simple features file
-	metro.name <- tolower( cbsa.name )
-	metro.name <- gsub( ",", "", metro.name )
-	metro.name <- gsub( " ", "-", metro.name )
-	file.name <- paste0( "../maps/sf-metros/", metro.name, "-sf.geojson" )
-	write_sf( d.sf, file.name, delete_dsn=TRUE )
 
 	# convert sf to sp class
 	d.sf <- d.sf[ ! st_is_empty( d.sf ) , ]
 	d.sp <- as_Spatial( d.sf )
 
+	# project map and remove empty tracts
+	d.sp <- spTransform( d.sp, CRS("+proj=longlat +datum=WGS84") )
+	d.sp  <- d.sp[ d.sp$POP != 0 & (! is.na( d.sp$POP )) , ]
+
+	# save simple features shapefile as geojson
+	metro.name <- tolower( cbsa.name )
+	metro.name <- gsub( ",", "", metro.name )
+	metro.name <- gsub( " ", "-", metro.name )
+	file.name <- paste0( "../maps/metros-shapefile/", metro.name, "-sf.geojson" )
+	geojson_write( d.sp, file=file.name, geometry="polygon", delete_dsn=TRUE )
+
 	# class( d.sp )
 	# plot( d.sp)
 
 	# project map and remove empty tracts
-	d.sp <- spTransform( d.sp, CRS("+init=epsg:3395") )
-	d.sp  <- d.sp[ d.sp$POP != 0 & (! is.na( d.sp$POP )) , ]
+	# d.sp <- spTransform( d.sp, CRS("+init=epsg:3395") )
+	# d.sp  <- d.sp[ d.sp$POP != 0 & (! is.na( d.sp$POP )) , ]
 
 	# convert census tract polygons to dorling cartogram
 	# no idea why k=0.03 works, but it does - default is k=5
-	# max(msp.sp$POP)   # standardizes it to max of 1.5
-	d.sp$pop.w <- d.sp$POP / 9000 
-	dorling.map <- cartogram_dorling( x=d.sp, weight="pop.w", k=0.05 )
+	# standardizes pop numbers for scaling
+	d.sp$pop.w <- d.sp$POP / ( 2 * median(d.sp$POP) )  # 9000
+	d.sp <- spTransform( d.sp, CRS("+init=epsg:3395") )
+	dorling.map <- cartogram_dorling( x=d.sp, weight="pop.w", k=0.2 )  # k=0.05
 
-        # save dorling shapefile as geojson
+	# convert to WGS84 for GitHub capatability 
+	metro.j <- spTransform( dorling.map, CRS("+proj=longlat +datum=WGS84") )
+
+	# save dorling shapefile as geojson
 	metro.name <- tolower( cbsa.name )
 	metro.name <- gsub( ",", "", metro.name )
 	metro.name <- gsub( " ", "-", metro.name )
-	file.name <- paste0( "../maps/dorling-metros/", metro.name, "-dorling.geojson" )
-	metro.j <- spTransform( dorling.map, CRS("+proj=longlat +datum=WGS84") )
+	file.name <- paste0( "../maps/metros-dorling/", metro.name, "-dorling.geojson" )
+
 	geojson_write( metro.j, file=file.name, geometry="polygon", delete_dsn=TRUE )
 
-	# plot the map 
-	plot( dorling.map, main=paste0("Dorling Cartogram of \n", cbsa.name ) )
+	# plot the maps 
+	if( include.plot )
+	{
+	  par( mar=c(0,0,4,0), mfrow=c(1,2) )
+	  plot( d.sp, main=paste0("Census Tracts of \n", cbsa.name ) )
+	  plot( dorling.map, main=paste0("Dorling Cartogram of \n", cbsa.name ) )
+	}
 
 	return( dorling.map )
 
 }
+
 
 
 # test the function: 
